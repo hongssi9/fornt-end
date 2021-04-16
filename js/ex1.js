6e61135f6f640e23e42f6c5886de486ea9c5af9e
hongssi9 <vvv9605@naver.com>
2021-04-16 19:27:51 +0900

    자바스크립트 선택과 window.onload -> window.~~~ine("onload",

--- js/ex1.js
+++ js/ex1.js
@@ -1,41 +1,75 @@
-// ---------------------chiledNodees를 이용한 노드 선택-------------------------
-// window.onload = function(){
-//     var section4 = document.querySelector("#section4");
-//     var box = section4.querySelector(".box");
-//     // section4 는 document객체를 사용하여 id section4에 공간을 가져왔다
-//     // document로 이미 웹페이지 자체가 되었으니 section4변수에있는 공간에서 class.box값을 가져 올 수 있다.
+window.addEventListener("load", function(){
+    var section = document.querySelector("#ex6");
 
-//     var input1 = box.children[0]; //box에 있는 자식 0번째를.. //자식을 찾을수 있는..             
-//     var input2 = box.children[1]; //childNodes 빈공백이 들어가면 그 빈 공간도 자식으로....
-//                                   //chtledren은 태그형태로 된것만 자식으로 본다.
-//     input1.value = "hello";
-//     input2.value = "okay";
+    var container_ = section.querySelector(".container");
+    var boxes = section.querySelectorAll(".box"); //All은 목록으로
+    var button = section.querySelector("input[value='click']");
+    
+ 
+});
 
-//  };
+
+
+window.addEventListener("load", function(){
+    var section = document.querySelector("#ex5");
+
+    var span_ = section.querySelector(".span");
+    var btn = section.querySelector(".btn");
+    
+    btn.onclick = function(){
+      
+        window.setInterval(function(){  //setInterval 누르면 1초마다 계속 실행...
+            var a = parseInt(span_.innerText);
+            a--;
+            span_.innerText = a;
+            console.log("test");
+        }, 1000);   //윈도우 기능중 시간을 설정할 수 있는...
+    }
+});
+
+
+
+
+
+
+
+// -------3번째--------------chiledNodees를 이용한 노드 선택-------------------------
+window.addEventListener("load",function(){
+    var section4 = document.querySelector("#section4");
+    var box = section4.querySelector(".box");
+    // section4 는 document객체를 사용하여 id section4에 공간을 가져왔다
+    // document로 이미 웹페이지 자체가 되었으니 section4변수에있는 공간에서 class.box값을 가져 올 수 있다.
+
+    var input1 = box.children[0]; //box에 있는 자식 0번째를.. //자식을 찾을수 있는..             
+    var input2 = box.children[1]; //childNodes 빈공백이 들어가면 그 빈 공간도 자식으로....
+                                  //chtledren은 태그형태로 된것만 자식으로 본다.
+    input1.value = "hello";
+    input2.value = "okay";
+
+ });
 
 
 
 //------------------------------선택------------------------------
-// window.onload = function(){ //정해진 함수명?
-//     var section = document.getElementById("ex3");
-//     var x_ = document.querySelector("input[name='text-x']"); //다른 방법 input태그에 name이 text-x라는걸...
-//     var y_ = document.querySelector(".text-y");//All은 여러개를 찾을때
-//     var sum_ = document.querySelector(".text-sum");
-//     var btn_ = document.querySelector(".submit-btn");
-//      //Document : 웹페이지 그 자체 HTML접근하려면 document객체부터..
+//window.onload = function(){ //정해진 함수명?
+window.addEventListener("load", function(){
+    var section = document.getElementById("ex3");
+    var x_ = document.querySelector("input[name='text-x']"); //다른 방법 input태그에 name이 text-x라는걸...
+    var y_ = document.querySelector(".text-y");//All은 여러개를 찾을때
+    var sum_ = document.querySelector(".text-sum");
+    var btn_ = document.querySelector(".submit-btn");
+     //Document : 웹페이지 그 자체 HTML접근하려면 document객체부터..
  
-//     btn_.onclick = function(){
+    btn_.onclick = function(){
       
-//         var x = parseInt(x_.value); //x 변수에 값을 정수형으로 변환하여 넣어준다
-//         var y = parseInt(y_.value);
+        var x = parseInt(x_.value); //x 변수에 값을 정수형으로 변환하여 넣어준다
+        var y = parseInt(y_.value);
  
-//         //sum_.innerText = x+y; //텍스트를 바꿀때는 innerText
-//         sum_.value = x+y;
+        //sum_.innerText = x+y; //텍스트를 바꿀때는 innerText
+        sum_.value = x+y;
          
-//      };
-     
- 
-//  };
+     };
+ });
 
 
 
@@ -61,7 +95,7 @@
 // };
 
 // ------------------------------------내가한거-------------------------------
-window.onload = function(){
+window.addEventListener("load", function(){
     var section = document.querySelector("#ex2");
     var x_ = section.querySelector(".text-x");
     var y_ = section.querySelector(".text-y");
@@ -73,7 +107,7 @@ window.onload = function(){
         var y = parseInt(y_.value);
         sum_.value = x+y;
     };
-};
+});
 
 
 
