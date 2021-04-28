d19bd7838895c424e8f5a603cb43b6d86a8d48f4
hongssi9 <vvv9605@naver.com>
2021-04-28 23:06:59 +0900

    회원가입 필수항목 설정

---
+++ test/Sign_Up.js
@@ -0,0 +1,16 @@
+var id = document.querySelector('#id');
+id.addEventListener("focusout", checkId);
+function checkId() {
+    var idPattern = /[a-zA-Z0-9_-]{5,20}/;
+    if(id.value === "") {
+        error[0].innerHTML = "필수 정보입니다.";
+        error[0].style.display = "block";
+    } else if(!idPattern.test(id.value)) {
+        error[0].innerHTML = "5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.";
+        error[0].style.display = "block";
+    } else {
+        error[0].innerHTML = "멋진 아이디네요!";
+        error[0].style.color = "#08A600";
+        error[0].style.display = "block";
+    }
+}
